c4c31f8d8888f364b97cf8fd60b8ff1a75ed764b
Eno Bassey <ennyboy@gmail.com>
2021-04-18 01:12:34 +0100

    - eject devise - add bootstrap - add classes to forms

--- spec/controllers/time_entries_controller_spec.rb
+++ spec/controllers/time_entries_controller_spec.rb
@@ -6,13 +6,14 @@ RSpec.describe TimeEntriesController, type: :controller do
         it { should use_before_action(:set_time_entry) }
     end
 
-    let(:user) { create(:user) }
-
-    before do
-        allow_any_instance_of(TimeEntriesController).to receive(:authenticate_user!).and_return(user)
-        allow_any_instance_of(TimeEntriesController).to receive(:current_user).and_return(user)
-      end
     describe "GET #index" do
+        
+        let(:user) { create(:user) }
+    
+        before do
+          allow_any_instance_of(TimeEntriesController).to receive(:authenticate_user!).and_return(user)
+          allow_any_instance_of(TimeEntriesController).to receive(:current_user).and_return(user)
+        end
     
         describe "When entries exist within the account" do
           let!(:time_entries) { create(:time_entry,  user: user) }
@@ -30,32 +31,9 @@ RSpec.describe TimeEntriesController, type: :controller do
         
     end
 
-    describe "POST start_timer" do
-
-        it "start timer for current user" do
-          expect(user.time_entries).to eq []
-          post :start_timer
-          expect(user.time_entries).not_to eq []
-        end
-
-        context "when timer is currently running" do
-            let!(:time_entry) { create(:time_entry,  user: user) }
-
-            it "should not allow a new timer" do
-              expect{ post :start_timer }.not_to change{TimeEntry.count}
-            end
-        end
-    end
-
-    describe "POST stop_timer" do
-        let!(:time_entry) { create(:time_entry,  user: user) }
     
-        it "stop_timer for current user" do
-          expect(time_entry.finishes_at).to eq nil
-          post :stop_timer
-          expect(time_entry.reload.finishes_at).not_to eq nil
-        end
-    end
+
+
 end
 
   
